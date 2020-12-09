02ef272dd3648acdba91c50f425a019a0d340cbc
bqfox <bq@6block.com>
2020-12-10 02:33:26 +0800

    export BufferReader

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -2,6 +2,7 @@ export { Buffer } from 'buffer';
 export { default as Address } from './address';
 export { default as AMT } from './amt';
 export { default as Bitfield } from './bitfield';
+export { default as BufferReader } from './buffer-reader';
 export { default as Cid } from './cid';
 export { default as decode } from './decoder';
 export { default as HAMT, HAMTKey, HAMTType } from './hamt';
--- src/node/index.ts
+++ src/node/index.ts
@@ -1,6 +1,7 @@
 export { default as Address } from './address';
 export { default as AMT } from './amt';
 export { default as Bitfield } from './bitfield';
+export { default as BufferReader } from './buffer-reader';
 export { default as Cid } from './cid';
 export { default as decode } from './decoder';
 export { default as HAMT, HAMTKey, HAMTType } from './hamt';
