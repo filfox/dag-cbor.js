5777f84b6c4ceb14f5923eff1e8f5058081d561e
bqfox <bq@6block.com>
2021-05-10 19:35:43 +0800

    AMT v3

--- src/node/amt.ts
+++ src/node/amt.ts
@@ -10,9 +10,10 @@ function bitCount(n: number) {
 }
 
 type ChainReadObj = (cid: Cid) => Buffer | Promise<Buffer>;
+export type AMTVersion = 2 | 3;
 
 export class AMTType<T extends ParamType> {
-  constructor(public readonly Type: T) {}
+  constructor(public readonly Type: T, public readonly version: AMTVersion) {}
 }
 
 class AMTNode<T extends ParamType> {
@@ -34,6 +35,7 @@ class AMTNode<T extends ParamType> {
 
 export default class AMT<T extends ParamType> {
   private constructor(
+    public readonly BitWidth: number,
     public readonly Height: number,
     public readonly Count: number,
     public readonly Node: AMTNode<T>,
@@ -45,20 +47,30 @@ export default class AMT<T extends ParamType> {
   }
 
   static fromBufferReader<T extends ParamType>(reader: BufferReader, type: AMTType<T>) {
-    assert.deepStrictEqual(reader.readHeader(), { majority: 4, extra: 3 });
-    const height = reader.readNumber();
-    const count = reader.readNumber();
-    const node = AMTNode.fromBufferReader(reader, type);
-    return new AMT<T>(height, count, node, type);
+    if (type.version === 2) {
+      assert.deepStrictEqual(reader.readHeader(), { majority: 4, extra: 3 });
+      const height = reader.readNumber();
+      const count = reader.readNumber();
+      const node = AMTNode.fromBufferReader(reader, type);
+      return new AMT<T>(3, height, count, node, type);
+    } else {
+      assert.deepStrictEqual(reader.readHeader(), { majority: 4, extra: 4 });
+      const bitWidth = reader.readNumber();
+      const height = reader.readNumber();
+      const count = reader.readNumber();
+      const node = AMTNode.fromBufferReader(reader, type);
+      return new AMT<T>(bitWidth, height, count, node, type);
+    }
   }
 
   async get(index: number, chainReadObj: ChainReadObj) {
-    if (index >= 2 ** ((this.Height + 1) * 3)) {
+    const width = 2 ** this.BitWidth;
+    if (index >= width ** (this.Height + 1)) {
       return;
     }
     let node = this.Node;
     for (let height = this.Height; height >= 0; --height) {
-      const subIndex = Math.floor(index / 2 ** (height * 3)) % 8;
+      const subIndex = Math.floor(index / width ** height) % width;
       if ((node.Bmap[0] & 1 << subIndex) === 0) {
         return;
       }
